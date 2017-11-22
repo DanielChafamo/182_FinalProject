d0dde00343039e7bcd80a3818ea9fd90ec212690
DanielC <danielchafamo@college.harvard.edu>
2017-11-22 16:56:07 -0500

    digit_classifier update

--- digit_classifier.py
+++ digit_classifier.py
@@ -4,6 +4,18 @@ from naive_bayes import NaiveBayes
 with gzip.open('data/mnist.pkl.gz', 'rb') as f:
     train_set, valid_set, test_set = cPickle.load(f)
 
+num_train = 500
+num_test = 500
+alpha = .1
+
+train_data, train_labels = train_set[0][:num_train], train_set[1][:num_train]
+test_data, test_labels = test_set[0][:num_test], test_set[1][:num_test]
+
 multinomial_classifier = NaiveBayes(10, 28*28, 'm')
-multinomial_classifier.train(train_set[0][:1000], train_set[1][:1000], 0.1)
-print(multinomial_classifier.test(test_set[0][:1000], test_set[1][:1000]))
+multinomial_classifier.train(train_data, train_labels, alpha)
+
+predictions, accuracy = multinomial_classifier.test(test_data, test_labels)
+
+print(zip(predictions, test_labels))
+print('alpha: ', alpha)
+print('accuracy: ', accuracy)
